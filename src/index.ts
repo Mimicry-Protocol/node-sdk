9e9c728dbdd74c38b95dd0fac0a70454d3c4b230
Aslan Goldenhour <aslan@mimicry.finance>
2023-05-21 19:26:37 -0400

    set default imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-export const sum = (a: number, b: number) => {
-  if (__DEV__) {
-    console.log('bloop bloop');
-  }
-
-  return a + b;
-};
+export * from './lib/mimicry/mimicrySdk';
+export * from './lib/utils/sum';
