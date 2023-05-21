1a4861d473fd0fde4fa1e73d75ff630feff7e965
Aslan Goldenhour <aslan@mimicry.finance>
2023-05-21 19:24:30 -0400

    fleshing out the example folder

--- example/index.js
+++ example/index.js
@@ -1,3 +1,12 @@
-import { sum } from '../dist/index.js';
+import { MimicrySDK, sum } from '../dist/index.js';
 
 console.log(sum(1, 2));
+
+import { MimicrySDK } from '../dist/index.js';
+import { ethers } from "ethers";
+
+const privateKey = process.env.PRIVATE_KEY;
+const providerUrl = process.env.PROVIDER_URL;
+const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+const signer = new ethers.Wallet(privateKey, provider);
+const mimicry = new MimicrySDK(signer, 80001);
