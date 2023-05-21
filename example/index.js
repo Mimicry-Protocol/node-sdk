b4f90d8287cc8707fbdf22156ad6bddc476edc22
Aslan Goldenhour <aslan@mimicry.finance>
2023-05-21 19:45:01 -0400

    fixed the setup for ethers

--- example/index.js
+++ example/index.js
@@ -1,12 +1,15 @@
 import { MimicrySDK, sum } from '../dist/index.js';
-
-console.log(sum(1, 2));
-
-import { MimicrySDK } from '../dist/index.js';
 import { ethers } from "ethers";
+import 'dotenv/config';
 
 const privateKey = process.env.PRIVATE_KEY;
 const providerUrl = process.env.PROVIDER_URL;
-const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+if (!privateKey || !providerUrl) {
+    throw new Error('Please set PRIVATE_KEY and PROVIDER_URL in the .env file');
+}
+
+const provider = new ethers.JsonRpcProvider(providerUrl);
 const signer = new ethers.Wallet(privateKey, provider);
 const mimicry = new MimicrySDK(signer, 80001);
+
+console.log(sum(1, 2));
