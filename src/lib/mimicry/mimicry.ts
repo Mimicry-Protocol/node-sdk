b05745ffa4475a8b4756cb62d2995feed4309c67
Aslan Goldenhour <aslan@mimicry.finance>
2023-05-22 02:27:42 -0400

    Refactored Mimicry class

--- src/lib/mimicry/mimicry.ts
+++ src/lib/mimicry/mimicry.ts
@@ -1,52 +1,57 @@
 import { Signer, Contract } from 'ethers';
 import * as MimicryABI from './abi/mimicry.json';
 import { Market } from './market';
-// import { MarketInfo } from '../types';
+import { Currency } from './currency';
+import { ChainId } from '../enums';
 
 export class Mimicry {
   private signer: Signer;
   private contract: Contract;
 
-  constructor(_signer: Signer, _network: number) {
+  constructor(_signer: Signer, _network: ChainId) {
     this.signer = _signer;
 
-    this.contract = new Contract(
-      '0x3fF86EEc088ea07464E5329db10f94AC859be633',
-      MimicryABI.abi as any,
-      _signer
-    );
+    const _address =
+      _network === ChainId.POLYGON_POS
+        ? '0x5aeDa339237eE646aFbDbc7E9E9eBab15f7660D9' // Mainnet
+        : '0x3fF86EEc088ea07464E5329db10f94AC859be633'; // Mumbai
+
+    this.contract = new Contract(_address, MimicryABI.abi as any, _signer);
 
     if (__DEV__) {
-      console.log('MimicrySDK constructor');
+      console.log('Mimicry Constructor');
     }
-  }
-
-  public async getMarkets() {
-    const markets = await this.contract.getMarkets();
-    let marketInfos = [];
-    for (const address of markets) {
-      const marketInfo = new Market(address, this.signer);
-      marketInfos.push(marketInfo);
-    }
-    return marketInfos;
   }
 
   // ---- MARKETS ---------------------------------------------------------------
-  // public async addMarket(_info: MarketInfo): Promise<Market> {
-  //     return new Market(this.signer, _info);
-  // }
-  // public async getMarkets(): Promise<Market[]> {
-  //     const markets = await this.factory.getMarkets();
-  //     return [];
-  // }
-  // public async getMarket(_address: string): Promise<Market> {
-  //     const marketInfo = await this.factory.getMarkets();
-  //     return Market;
-  // }
+  public async getMarkets(): Promise<Market[]> {
+    const marketAddresses = await this.contract.getMarkets();
+    let markets = [];
+    for (const address of marketAddresses) {
+      const market = await this.getMarket(address);
+      markets.push(market);
+    }
+    return markets;
+  }
+
+  public async getMarket(_address: string): Promise<Market> {
+    return await Market.initialize(_address, this.signer);
+  }
 
   // ---- CURRENCIES ------------------------------------------------------------
-  // public async getCurrencies(): Promise<Currency[]> { return []; }
-  // public async getCurrency(_address: string): Promise<Currency> {}
+  public async getCurrencies(): Promise<Currency[]> {
+    const currencyAddresses = await this.contract.getValidCurrenciesList();
+    let currencies = [];
+    for (const address of currencyAddresses) {
+      const currency = await this.getCurrency(address);
+      currencies.push(currency);
+    }
+    return currencies;
+  }
+
+  public async getCurrency(_address: string): Promise<Currency> {
+    return await Currency.initialize(_address, this.signer);
+  }
 
   // ---- PLAYERS ---------------------------------------------------------------
   // public async getPlayers(): Promise<Player[]> { return []; }
