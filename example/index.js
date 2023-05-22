edf168780cfee0e8efcb56a3b8807341ab09c89d
Aslan Goldenhour <aslan@mimicry.finance>
2023-05-21 22:41:11 -0400

    fix in example

--- example/index.js
+++ example/index.js
@@ -2,19 +2,19 @@ import { MimicrySDK, sum } from '../dist/index.js';
 import { ethers } from "ethers";
 import 'dotenv/config';
 
-const privateKey = process.env.PRIVATE_KEY;
-const providerUrl = process.env.PROVIDER_URL;
-if (!privateKey || !providerUrl) {
-    throw new Error('Please set PRIVATE_KEY and PROVIDER_URL in the .env file');
-}
-
 try {
+    const privateKey = process.env.PRIVATE_KEY;
+    const providerUrl = process.env.PROVIDER_URL;
+    if (!privateKey || !providerUrl) {
+        throw new Error('Please set PRIVATE_KEY and PROVIDER_URL in the .env file');
+    }
+
     const provider = new ethers.JsonRpcProvider(providerUrl);
     const signer = new ethers.Wallet(privateKey, provider);
     const mimicry = new MimicrySDK(signer, 80001);
 
     console.log(await mimicry.factory.getMarkets());
-    // console.log(sum(1, 2));
+    console.log(sum(1, 2));
 } catch (error) {
     console.log(error.message);
 }
