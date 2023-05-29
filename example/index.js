e4282d00db04611214d240d8255edb6908860d27
Aslan Goldenhour <aslan@mimicry.finance>
2023-05-29 03:53:06 -0400

    added getReferenceValues() on a market

--- example/index.js
+++ example/index.js
@@ -14,19 +14,21 @@ try {
     const signer = new ethers.Wallet(privateKey, provider);
     const mimicry = new Mimicry(signer, ChainId.MUMBAI);
 
-    const mockUsdc = await mimicry.getCurrency('0x7bc699a078b3A6FD386835b3b5C9a10BF53f894C');
-    console.log(await mockUsdc.getInfo());
+    // const mockUsdc = await mimicry.getCurrency('0x7bc699a078b3A6FD386835b3b5C9a10BF53f894C');
+    // console.log(await mockUsdc.getInfo());
     
     const remilio = await mimicry.getMarket('0x1887c38229e4f87768fddc847ce70dec7ddf7cb6');
     console.log(await remilio.getInfo());
 
-    console.log("\nPosition Value:");
-    console.log(await remilio.getPositionValue(28));
+    console.log(await remilio.getReferenceValues());
+
+    // console.log("\nPosition Value:");
+    // console.log(await remilio.getPositionValue(28));
 
     // console.log(await milady.closePosition(14));
     // console.log(await remilio.openPosition(Direction.SHORT, mockUsdc, 13.75));
 
-    console.log(await remilio.commitValueTransfer());
+    // console.log(await remilio.commitValueTransfer());
 
 } catch (error) {
     console.log(error);
